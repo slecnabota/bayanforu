9b18a3d5e4ec4a1330e378c2e061adc743da3147
Akbota <85954194+slecnabota@users.noreply.github.com>
2023-04-14 09:50:53 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.s1-swiper', {
     direction: 'horizontal',
     loop: true,
     autoplay: {
         delay: 3000,
     },
-  });
+});
+const swiper2 = new Swiper('.s4-swiper', {
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 3000,
+    },
+});
+const swiper5 = new Swiper('.s5-swiper', {
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 3000,
+    },
+});
